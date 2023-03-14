5b5fa0215cb011efce47ad43806c04a2d021ccfb
Kevin Gislason <kevingislason@gmail.com>
2023-03-14 01:47:06 -0400

    remove unused file

--- src/utils/display/getUsdValueString.ts
+++
@@ -1,8 +0,0 @@
-import Usd from 'types/Usd';
-
-const getUsdDisplayString = (value: Usd) => {
-  const usdValue = value / 100;
-  return `$${usdValue.toFixed(2)}`;
-};
-
-export default getUsdDisplayString;
