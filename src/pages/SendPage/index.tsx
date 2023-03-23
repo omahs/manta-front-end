aafff65869a5ead8a6b7ca25c27196bc2984d51a
Jason <jasonintju@gmail.com>
2023-03-23 14:49:36 +0800

    remove downtime for dolphin (#824)

--- src/pages/SendPage/index.tsx
+++ src/pages/SendPage/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import PageContent from 'components/PageContent';
 import Navbar from 'components/Navbar';
 import IPBlockingModal from 'components/Modal/IPBlockingModal';
-import { useConfig } from 'contexts/configContext';
-import { dolphinConfig } from 'config';
-import DowntimeModal from 'components/Modal/downtimeModal';
+// import { useConfig } from 'contexts/configContext';
+// import { dolphinConfig } from 'config';
+// import DowntimeModal from 'components/Modal/downtimeModal';
 import { SendContextProvider } from './SendContext';
 import { PrivateTxHistoryContextProvider } from './privateTxHistoryContext';
 import SendForm from './SendForm';
 
 const SendPage = () => {
-  const { NETWORK_NAME } = useConfig();
-  const isDolphinPage = NETWORK_NAME === dolphinConfig.NETWORK_NAME;
+  // const { NETWORK_NAME } = useConfig();
+  // const isDolphinPage = NETWORK_NAME === dolphinConfig.NETWORK_NAME;
   return (
     <SendContextProvider>
       <PrivateTxHistoryContextProvider>
@@ -20,7 +20,7 @@ const SendPage = () => {
           <SendForm />
         </PageContent>
         <IPBlockingModal />
-        { isDolphinPage && <DowntimeModal />}
+        {/* { isDolphinPage && <DowntimeModal />} */}
       </PrivateTxHistoryContextProvider>
     </SendContextProvider>
   );
