382a0f2ed6ef804180f28c6c9575a84598562081
chan <jincan39@163.com>
2023-04-08 08:06:34 +0800

    chore: rm Dolphin DowntimeModal

--- src/pages/SendPage/index.tsx
+++ src/pages/SendPage/index.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import PageContent from 'components/PageContent';
-import Navbar from 'components/Navbar';
 import IPBlockingModal from 'components/Modal/IPBlockingModal';
-import { useConfig } from 'contexts/configContext';
-import { dolphinConfig } from 'config';
-import DowntimeModal from 'components/Modal/downtimeModal';
+import Navbar from 'components/Navbar';
+import PageContent from 'components/PageContent';
 import { SendContextProvider } from './SendContext';
-import { PrivateTxHistoryContextProvider } from './privateTxHistoryContext';
 import SendForm from './SendForm';
+import { PrivateTxHistoryContextProvider } from './privateTxHistoryContext';
 
 const SendPage = () => {
-  const { NETWORK_NAME } = useConfig();
-  const isDolphinPage = NETWORK_NAME === dolphinConfig.NETWORK_NAME;
+
   return (
     <SendContextProvider>
       <PrivateTxHistoryContextProvider>
@@ -20,7 +15,6 @@ const SendPage = () => {
           <SendForm />
         </PageContent>
         <IPBlockingModal />
-        { isDolphinPage && <DowntimeModal />}
       </PrivateTxHistoryContextProvider>
     </SendContextProvider>
   );
