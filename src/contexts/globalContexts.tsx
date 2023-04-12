d8bfc5cc325bc49582440a741b0b1b0f697d90bd
chan <jincan39@163.com>
2023-04-12 19:06:50 +0800

    fix: keep usingMantaWallet state

--- src/contexts/globalContexts.tsx
+++ src/contexts/globalContexts.tsx
@@ -1,32 +1,47 @@
 // @ts-nocheck
 import { localStorageKeys } from 'constants/LocalStorageConstants';
-import React, { createContext, useContext, useCallback, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState
+} from 'react';
 import store from 'store';
 import { KeyringContextProvider } from './keyringContext';
 import { ThemeProvider } from './themeContext';
 
 const GlobalContext = createContext();
 
-const GlobalContextProvider = ({children}) => {
-  const [usingMantaWallet, _setUsingMantaWallet] = useState(store.get(localStorageKeys.UsingMantaWallet) || true);
+const GlobalContextProvider = ({ children }) => {
+  const [usingMantaWallet, _setUsingMantaWallet] = useState(true);
+  const _usingMantaWallet = store.get(localStorageKeys.UsingMantaWallet);
+
+  useEffect(() => {
+    if (_usingMantaWallet !== undefined) {
+      _setUsingMantaWallet(_usingMantaWallet);
+    }
+  }, [_usingMantaWallet]);
 
   const setUsingMantaWallet = useCallback((state) => {
     _setUsingMantaWallet(state);
     store.set(localStorageKeys.UsingMantaWallet, state);
   }, []);
 
-  const contextValue = useMemo(() => ({
-    usingMantaWallet,
-    setUsingMantaWallet
-  }), [usingMantaWallet, setUsingMantaWallet]);
+  const contextValue = useMemo(
+    () => ({
+      usingMantaWallet,
+      setUsingMantaWallet
+    }),
+    [usingMantaWallet, setUsingMantaWallet]
+  );
 
   return (
     <GlobalContext.Provider value={contextValue}>
       <KeyringContextProvider>
-        <ThemeProvider>
-          {children}
-        </ThemeProvider >
+        <ThemeProvider>{children}</ThemeProvider>
       </KeyringContextProvider>
     </GlobalContext.Provider>
   );
@@ -37,8 +52,7 @@ GlobalContextProvider.propTypes = {
 };
 
 export const useGlobal = () => ({
-  ...useContext(GlobalContext),
+  ...useContext(GlobalContext)
 });
 
 export default GlobalContextProvider;
-
