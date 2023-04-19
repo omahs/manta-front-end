640a39ba065e2f375a138017c4f65630abd1cf12
Kevin Gislason <kevingislason@gmail.com>
2023-04-18 22:11:19 -0400

    remove unused validation

--- src/pages/SendPage/SendButton.tsx
+++ src/pages/SendPage/SendButton.tsx
@@ -105,8 +105,6 @@ const ValidationSendButton = ({ showModal }) => {
     && versionIsOutOfDate(config.MIN_REQUIRED_WALLET_VERSION, extensionVersion)
   ) {
     validationMsg = 'Manta Wallet out of date';
-  } else if (versionIsOutOfDate(config.MIN_REQUIRED_SIGNER_VERSION, signerVersion)) {
-    validationMsg = 'Signer out of date';
   } else if (!externalAccount || (usingMantaWallet && !privateWallet && !isPublicTransfer())) {
     shouldShowWalletMissingValidation = true;
   } else if (usingMantaWallet && hasFinishedInitialBlockDownload === false) {
